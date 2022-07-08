d1798d86e4f76c2682b9eb4057efeebd4eb383a7
Vadim <vadkopitsa@gmail.com>
2022-07-08 14:58:21 +0300

    + login_page.py update locators.py and test_main_page.py

---
+++ pages/locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_PAGE = (By.ID, "login_form")
+    REGISTER_PAGE = (By.ID, "register_form")
