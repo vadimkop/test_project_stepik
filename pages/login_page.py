0e9640e0695a694d6b91f9187e7f18880a490d59
Vadim <vadkopitsa@gmail.com>
2022-07-08 15:27:06 +0300

    update design of tests and test_main_page.py (test_guest_can_go_to_login_page)

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,7 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
     def should_be_login_url(self):
         assert "login" in self.browser.current_url, "Login url is not presented"
 
@@ -11,3 +12,8 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_PAGE), "Register form is not presented"
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 
+
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
